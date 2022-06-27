c5f18a4fb561ee8e61675611433c7e16f92165ed
Stefan Mastilak <stefan.mastilak@visma.com>
2022-06-27 08:58:51 +0200

    'updating_notebooks'

--- notebooks/notebooks_py/sklearn_introduction.py
+++ notebooks/notebooks_py/sklearn_introduction.py
@@ -27,13 +27,13 @@
 # 6) Save and load a trained model
 # 7) Putting it all together
 
-# ## 0. End to end Scikit-Learn workflow
+# ### 1) Get the data ready
 
-# 1) Get the data ready:
+# 1) Get the data:
+import numpy as np
 import pandas as pd
 heart_disease = pd.read_csv('data/heart-disease.csv')
 heart_disease
-
 
 # +
 # create x (features matrix)
@@ -43,7 +43,73 @@ x = heart_disease.drop('target', axis=1)
 y = heart_disease['target']
 # -
 
-# 2) Choose the right model and hyperparameters
+# ### 2) Choose the right model and hyperparameters
+
+# +
+# import classification machine learning model from sklearn
+
 from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=100)
+
+# We'll keep the default hyperparameters
+clf.get_params()
+# -
+
+# ### 3) Fit the model to the training data
+
+# +
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+# -
+
+clf.fit(x_train, y_train);
+
+y_preds = clf.predict(x_test)
+y_preds
+
+# ### 4) Evaulate the model on training data and test data
+
+# check model score on training data
+clf.score(x_train, y_train)
+
+# check model score on test data
+clf.score(x_test, y_test)
+
+# +
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+print(classification_report(y_test, y_preds))
+# -
+
+confusion_matrix(y_test, y_preds)
+
+accuracy_score(y_test, y_preds)
+
+# ### 5) Improve a model
+#
+# Try different amount of n_estimators
+
+# +
+np.random.seed(42)
+
+for i in range(10,100,10):
+    print(f'Tryting model with {i} estimators..')
+    clf = RandomForestClassifier(n_estimators=i).fit(x_train, y_train)
+    print(f'Model accuracy on test set: {(clf.score(x_test, y_test) * 100).round(2)}%')
+    print("")
+
+# -
+
+# ### 6) Save a model and load it
+
+# +
+import pickle
+
+pickle.dump(clf, open('models/random_forest_model_1.pkl', 'wb'))
+# -
+
+loaded = pickle.load(open('models/random_forest_model_1.pkl', 'rb'))
+loaded.score(x_test, y_test)
 
 
